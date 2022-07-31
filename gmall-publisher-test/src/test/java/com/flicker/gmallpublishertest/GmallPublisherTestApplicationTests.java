274e9caf995f279bca529b5a43698fceac32c25e
flickever <1490757045>
2022-07-31 12:05:53 +0800

    对接sugar

---
+++ gmall-publisher-test/src/test/java/com/flicker/gmallpublishertest/GmallPublisherTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flicker.gmallpublishertest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallPublisherTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
